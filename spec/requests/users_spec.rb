7a7e0dc939378869527ddd39e733fdc85ab4a13c
Rafael Chacón <rafaelchacon@gmail.com>
2015-10-20 23:09:44 -0700

    Add logic to authorize users.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Users", type: :request do
   let(:user) { create(:user, password: '123456') }
 
   it "fetches an user by uuid" do
-    get "/v1/users/#{user.uuid}"
+    get "/v1/users/#{user.uuid}", nil, { 'X-Authenticated-Userid' => user.uuid }
     expect(response.status).to eq(200)
     user = User.last
     expect(json["data"]).to_not be_nil
@@ -17,8 +17,16 @@ RSpec.describe "Users", type: :request do
     expect(user_response_attributes["username"]).to eq(user.username)
   end
 
+  it "doesn't allow user to see other users profile" do
+    user_2 = create(:user, password: '123456')
+    get "/v1/users/#{user_2.uuid}", nil, { 'X-Authenticated-Userid' => user.uuid }
+    failed_error = ApiError::Unauthorized.new(I18n.t("errors.messages.unauthorized"))
+    expect(response.status).to eq(failed_error.status)
+    expect(json).to have_error_json_as(failed_error)
+  end
+
   it "responds with valid json on errors" do
-    get "/v1/users/invalid_id"
+    get "/v1/users/invalid_id", nil, { 'X-Authenticated-Userid' => "invalid_id" }
     failed_error = ApiError::NotFound.new(I18n.t("user.errors.not_found"))
     expect(response.status).to eq(failed_error.status)
     expect(json).to have_error_json_as(failed_error)
