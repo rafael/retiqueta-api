d38760e5af203eb4ba913acb575d6e8432d9463a
Rafael Chacón <rafaelchacon@gmail.com>
2015-11-03 21:25:16 -0800

    Set up basic elastic search.

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,4 +1,10 @@
 class V1::ProductsController < ApplicationController
+
+  def search
+    outcome = ::Products::Search.call(params)
+    render json: outcome.success_result, each_serializer: ProductSerializer, status: 200
+  end
+
   def create
     outcome = ::Products::Create.call(user_id:  user_id, data: create_product_params)
     render json: outcome.success_result, serializer: ProductSerializer, status: 201
