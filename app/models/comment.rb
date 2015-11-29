6df7eb51ecf7e5862b70d5379d94fa044a262d49
Rafael Chacón <rafaelchacon@gmail.com>
2015-11-29 14:10:21 -0800

    Add service to add comments to products.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,20 @@
 class Comment < ActiveRecord::Base
+
+  ##################
+  ## associations ##
+  ##################
+
   belongs_to :user, primary_key: :uuid
+
+  ###############
+  ## Callbacks ##
+  ###############
+
+  before_create :generate_uuid
+
+  private
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
