7ce6147b36f1e7cb747fc3d7350ab3ce9bc32b9c
Diorman Colmenares <dextrin.diorman@gmail.com>
2016-01-18 23:23:49 -0430

    Set up mailer for password resets

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,18 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.email)
   end
+
+  def password_reset_email(user)
+    @user = user
+    mail(to: @user.email)
+  end
+
+
+  private
+
+  helper do
+    def password_reset_url(user)
+      "https://retiqueta.com/password_resets?token=#{user.password_reset_token}"
+    end
+  end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -22,4 +22,27 @@ RSpec.describe UserMailer do
       expect(mail.body.encoded).to match(user.username)
     end
   end
+
+  describe ".send_password_reset_email" do
+    let(:token) { "abc" }
+    let(:user) { build(:user, password_reset_token: token) }
+    let(:mail) { UserMailer.password_reset_email(user) }
+
+    it "sets subject with i18n convention" do
+      expect(mail.subject).to eq(I18n.t("user_mailer.password_reset_email.subject"))
+    end
+
+    it "sets sender to default" do
+      expect(mail.from).to eq([ApplicationMailer::SENDER_ADDRESS])
+    end
+
+    it "sets user as receiver" do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it "includes password reset link in body" do
+      url = /https\:\/\/retiqueta.com\/password_resets\?token\=abc/
+      expect(mail.body.raw_source).to match(url)
+    end
+  end
 end
