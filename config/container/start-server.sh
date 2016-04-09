e0b93671b9c1341e0146770adc52bd7394a3e4d6
Rafael Chacón <rafaelchacon@gmail.com>
2016-04-09 14:20:15 -0700

    Test to setup kong

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd /app
 eval "$(bundle exec rake secrets:export)"
+dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis
+eval "$(bundle exec rake kong:setup)"
 bundle exec unicorn -p 3000 -c ./config/unicorn.rb
