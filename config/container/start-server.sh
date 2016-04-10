792cda4b9b68d073fb0e79e3c609530e2d8bc60b
Rafael Chacón <rafaelchacon@gmail.com>
2016-04-10 11:20:22 -0700

    Add bigger timeout to dockerized

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 cd /app
 eval "$(bundle exec rake secrets:export)"
-dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis
-mkdir -p /etc/ssl/kong
-echo $RETIQUETA_SSL_API_PEM > /etc/ssl/kong/api.retiqueta.pem
-echo $RETIQUETA_SSL_API_KEY > /etc/ssl/kong/api.retiqueta.key
+dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis --timeout 360s
 eval "$(bundle exec rake kong:setup)"
 bundle exec unicorn -p 3000 -c ./config/unicorn.rb
