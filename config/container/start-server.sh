c99da3cdf8cff9cadc0d5709e710af07af2b6cef
Rafael Chacón <rafaelchacon@gmail.com>
2016-04-09 19:22:58 -0700

    Use ssl cert from secrets

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -2,5 +2,8 @@
 cd /app
 eval "$(bundle exec rake secrets:export)"
 dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis
+mkdir -p /etc/ssl/kong
+echo $RETIQUETA_SSL_API_PEM > /etc/ssl/kong/api.retiqueta.pem
+echo $RETIQUETA_SSL_API_KEY > /etc/ssl/kong/api.retiqueta.key
 eval "$(bundle exec rake kong:setup)"
 bundle exec unicorn -p 3000 -c ./config/unicorn.rb
