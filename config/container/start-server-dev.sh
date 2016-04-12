9fb60b4d8b5a9c95784381f179172ff51f8f55f3
Rafael Chacón <rafaelchacon@gmail.com>
2016-04-11 23:01:44 -0700

    Add kong config file and env variables

--- config/container/start-server-dev.sh
+++ config/container/start-server-dev.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd /app
 bundle exec rake db:create
+bundle exec rake db:migrate
+dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis --timeout 360s
 eval "$(bundle exec rake kong:setup)"
 bundle exec unicorn -p 3000 -c ./config/unicorn.rb
