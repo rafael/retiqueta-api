c8597626e108fa70680b2580353ae8a9a5213b53
Rafael Chacón <rafaelchacon@gmail.com>
2016-06-22 20:39:58 -0700

    Add job to follow default users

---
+++ app/workers/account_bootstrap.rb
@@ -0,0 +1,17 @@
+class AccountBootstrap < ActiveJob::Base
+  queue_as :default
+
+  DEFAULT_USER_TO_FOLLOW = %w(johanna
+                              gaby
+                              norma
+                              rafael
+                              juanfa
+                              retiqueta)
+
+  def perform(user)
+    users_to_follow = User.where(username: DEFAULT_USER_TO_FOLLOW)
+    users_to_follow.each do |target_follow|
+      user.active_relationships.create!(followed_id: target_follow.uuid)
+    end
+  end
+end
