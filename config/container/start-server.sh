0487084751035ad450a92280400d2d461c5efc3a
Rafael Chacón <rafaelchacon@gmail.com>
2016-07-04 13:12:58 -0700

    Fix issue with librato and creds

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /app
-eval "$(bundle exec rake secrets:export)"
+eval "$(LIBRATO_AUTORUN=0 bundle exec rake secrets:export)"
 dockerize -wait http://${KONG_ADMIN_SERVICE_HOST}:${KONG_ADMIN_SERVICE_PORT}/apis --timeout 360s
 bundle exec unicorn -p 3000 -c ./config/unicorn.rb
