0222359a9828ec1805fdb2f4ca6613f291ded257
Rafael Chacón <rafaelchacon@gmail.com>
2016-07-17 22:58:51 -0700

    Add medium url to api response

--- app/serializers/product_picture_serializer.rb
+++ app/serializers/product_picture_serializer.rb
@@ -1,9 +1,13 @@
 class ProductPictureSerializer < ActiveModel::Serializer
 
-  attributes :id, :url, :small_url, :large_url, :product_id
+  attributes :id, :url, :small_url, :medium_url, :large_url, :product_id
 
   def large_url
     object.pic.url(:large)
+  end
+
+  def medium_url
+    object.pic(:small)
   end
 
   def small_url
