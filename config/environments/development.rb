8faa5cc2706449c34be9487155ec3e27c8a6d18a
Rafael Chacón <rafaelchacon@gmail.com>
2016-07-17 17:28:31 -0700

    Add CDN configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,21 +32,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.x.kong.external_host = ENV["DOCKER_HOST_IP"]
+  config.x.kong.external_host = ENV['DOCKER_HOST_IP']
   config.x.kong.external_port = 443
-  config.x.kong.internal_url = "https://kong:8443"
-  config.x.kong.users_ouath_token_path = "/v1/users/oauth2/token"
-  config.x.elastictsearch.host = "elasticsearch"
+  config.x.kong.internal_url = 'https://kong:8443'
+  config.x.kong.users_ouath_token_path = '/v1/users/oauth2/token'
+  config.x.elastictsearch.host = 'elasticsearch'
   config.x.elastictsearch.log = true
-  config.x.reset_password_url = "https://retiqueta.com/reset_password?token={{token}}"
+  config.x.reset_password_url = 'https://retiqueta.com/reset_password?token={{token}}'
 
   config.paperclip_defaults = {
     storage: :fog,
     fog_credentials:  {
-      provider: "AWS",
+      provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
     fog_directory: ENV['S3_BUCKET_NAME'],
+    fog_host: 'https://d2xhr02146786q.cloudfront.net'
   }
 end
