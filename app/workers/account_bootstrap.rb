6694b6caf20f55aebd1c6a92b95225f4bdd4c112
Rafael Chacón <rafaelchacon@gmail.com>
2016-09-28 08:19:12 -0700

    Change default people to follow

--- app/workers/account_bootstrap.rb
+++ app/workers/account_bootstrap.rb
@@ -2,16 +2,20 @@ class AccountBootstrap < ActiveJob::Base
   queue_as :default
 
   DEFAULT_USER_TO_FOLLOW = %w(johanna
+                              dianahernandezb
+                              ezra
+                              dianahernandezb
+                              rbriella
                               unatalluisa
-                              letterstolucia
+                              pattymendozah
                               anasalazarg
-                              camila
+                              carolavidalt2914
                               retiqueta)
 
   def perform(user)
     users_to_follow = User.where(username: DEFAULT_USER_TO_FOLLOW)
     users_to_follow.each do |target_follow|
-      #user.active_relationships.create!(followed_id: target_follow.uuid)
+      # user.active_relationships.create!(followed_id: target_follow.uuid)
       Users::Follow.call(follower_id: user.uuid,
                          followed_id: target_follow.uuid)
     end
