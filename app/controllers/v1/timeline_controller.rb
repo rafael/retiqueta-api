ebd13d13e4b1b9ae7061b7725a79038e2f8063c5
Rafael Chacón <rafaelchacon@gmail.com>
2016-11-07 18:36:32 -0800

    Make order desc in timeline

--- app/controllers/v1/timeline_controller.rb
+++ app/controllers/v1/timeline_controller.rb
@@ -5,7 +5,7 @@ class V1::TimelineController < ApplicationController
     page = params.fetch(:page) { {} }
     per_page = page[:size] || 25
     page_num = page[:number] || 1
-    outcome = Timeline::Card.all.page(page_num).per(per_page)
+    outcome = Timeline::Card.all.order(created_at: :desc).page(page_num).per(per_page)
     render json: outcome,
            status: 200
   end
