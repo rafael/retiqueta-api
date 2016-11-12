b233a8e74cfa65b50b04fcf09097f00dc7e85396
Rafael Chacón <rafaelchacon@gmail.com>
2016-11-12 14:01:10 -0800

    Remove focus

--- spec/workers/timeline_product_cleaner_spec.rb
+++ spec/workers/timeline_product_cleaner_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe TimelineProductCleaner, type: :job do
     Timeline::Card.create_featured_picks_card('Test', Product.last(3))
   end
 
-  it 'does nothing when no timeline cards match the id', :focus do
+  it 'does nothing when no timeline cards match the id' do
     TimelineProductCleaner.perform_now('dummy_id')
     expect(Timeline::Card.count).to eq(1)
   end
 
-  it 'does nothing when no timeline cards match the id', :focus do
+  it 'deletes product when a timeline card exists' do
     TimelineProductCleaner.perform_now(Product.last.uuid)
     expect(Timeline::Card.count).to eq(0)
   end
