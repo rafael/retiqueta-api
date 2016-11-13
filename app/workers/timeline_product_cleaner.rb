2ff16ebb2b8b7b0a39d909fea5b3bbe328695159
Rafael Chacón <rafaelchacon@gmail.com>
2016-11-12 18:48:32 -0800

    Fix some stuff

--- app/workers/timeline_product_cleaner.rb
+++ app/workers/timeline_product_cleaner.rb
@@ -3,7 +3,8 @@ class TimelineProductCleaner < ActiveJob::Base
 
   def perform(product_id)
     Timeline::Card
-      .where(card_type: Timeline::Card:: FEATURED_PICKS_TYPE)
+      .where(card_type: [Timeline::Card:: FEATURED_PICKS_TYPE,
+                         Timeline::Card::USER_LIKES_TYPE])
       .where("payload -> 'product_ids' ? :id ", id: product_id).destroy_all
   end
 end
