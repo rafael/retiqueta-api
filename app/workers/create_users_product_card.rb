cbad04284af7344e3c992dcbfa5feed0941760a5
Whitney Zack <wcz8292@gmail.com>
2016-11-20 12:37:21 -0800

    Create timeline cards for each follower of a user when they post items

--- app/workers/create_users_product_card.rb
+++ app/workers/create_users_product_card.rb
@@ -10,14 +10,14 @@ class CreateUsersProductCard < ActiveJob::Base
   private
 
   def create_card(user)
-    Timeline::Card.create_products_card(
-      title: user.username + ' ' + I18n.t('timeline_cards.users_product_card_title'),
-      products: products(user),
-      card_type: Timeline::Card::USER_PRODUCT_TYPE,
-      user_id: user.uuid)
+    @products ||= Product.where(user_id: user.id).last(2)
+    user.followers.each do |follower|
+      Timeline::Card.create_products_card(
+        title: user.username + ' ' + I18n.t('timeline_cards.users_product_card_title'),
+        products: @products,
+        card_type: Timeline::Card::USER_PRODUCT_TYPE,
+        user_id: follower.uuid)
+    end
   end
 
-  def products(user)
-    @products ||= Product.where(user_id: user.id)
-  end
 end
