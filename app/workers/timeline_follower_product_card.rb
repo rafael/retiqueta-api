7d30d1115d1b6bbd0721bdb5b42f665923bca480
Whitney Zack <wcz8292@gmail.com>
2016-11-21 16:26:23 -0800

    Fix indexing until after we count if their product numbers are even

--- app/workers/timeline_follower_product_card.rb
+++ app/workers/timeline_follower_product_card.rb
@@ -13,14 +13,14 @@ class TimelineFollowerProductCard < ActiveJob::Base
     user.followers.each do |follower|
       Timeline::Card.create_products_card(
         title: I18n.t('timeline_cards.users_product_card_title', username: user.username),
-        products: products(user),
+        products: products(user)[0..1],
         card_type: Timeline::Card::USER_LIKES_TYPE,
         user_id: follower.uuid)
     end
   end
 
   def products(user)
-    @products ||= Product.where(user_id: user.uuid).order(created_at: :desc)[0..1]
+    @products ||= Product.where(user_id: user.uuid).order(created_at: :desc)
   end
 
 end
